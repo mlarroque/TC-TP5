d0175424350ea86fb2d691fd3d81b4f88981a55b
Lu <lu.gfernandez@gmail.com>
2018-10-31 02:11:11 -0300

    resultados en proceso

--- rauch/componentes.m
+++ rauch/componentes.m
@@ -6,12 +6,9 @@ R1=82e3;
 C1=444e-12;
 C2=444e-12;
 
-% wo=141306;
-% r1=47e3;
-% c=680e-12;
 
 alpha=(qo-q)/(-2*q*qo^2);
-K=alpha/(1+alpha);
+k=alpha/(1+alpha);
 
 r=1/(3*C1*wo);
 a=r/R1;
@@ -19,4 +16,18 @@ R2=9*r;
 R3=r/(1-a);
 r6=k*r;
 r7=(1-k)*r;
-g=R3*R2*C1/((K-1)*R1*R3*(C1+C2)+K*(R1+R3)*R2*C2);
+
+wob=141306;
+R1b=47e3;
+C1b=680e-12;
+C2b=680e-12;
+
+alphab=(qo-q)/(-2*q*qo^2);
+kb=alphab/(1+alphab);
+
+rb=1/(3*C1b*wob);
+ab=rb/R1b;
+R2b=9*rb;
+R3b=rb/(1-ab);
+r6b=kb*rb;
+r7b=(1-kb)*rb;
